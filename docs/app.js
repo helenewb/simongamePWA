e6d312080bf95cf6fd97c0a09c8d7e7fbeca2382
Helene <1810738627@stud.fh-kufstein.ac.at>
2019-06-01 15:11:42 +0200

    animate sequence - wip

--- docs/app.js
+++ docs/app.js
@@ -1,6 +1,8 @@
 //initialise variables
 var gameBtns = document.querySelectorAll('.gamebtn');
 var startBtn = document.querySelector('#startbtn');
+var btnarray = ['#yellow', '#red', '#green', '#blue'];
+var testarray = [0, 1, 2, 3, 3, 0, 0];
 
 //Event listener for all colored buttons
 gameBtns.forEach(function (btn) {
@@ -13,6 +15,7 @@ gameBtns.forEach(function (btn) {
 //Event listener for Start button
 startBtn.addEventListener('click', () => {
     console.log(startBtn.id + ' läuft');
+    startAnimation();
 });
 
 //ANIMATION OF BUTTONS
@@ -21,4 +24,16 @@ function animation(item) {
     setTimeout(function () {
         item.classList.remove('brighten');
     }, 250);
+}
+
+function startAnimation() {
+    testarray.forEach(element => {
+        clickedBtn(element);
+        setTimeout(5000);
+        
+    });
+}
+
+function clickedBtn(i) {
+    document.querySelector(btnarray[i]).click();
 }
